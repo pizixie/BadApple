5e123285e864cc133b2c00be5c949c9661ca9630
spizi X <pizixie@hotmail.com>
2015-02-03 21:35:49 +0800

    实现DeviceLinkService。部分实现MobileSync。

--- BadApple/DeviceLinkService.cs
+++ BadApple/DeviceLinkService.cs
@@ -14,22 +14,38 @@ namespace BadApple
             this.sd = sd;
         }
 
-        public PListRoot VersionExchange(long major,long minor)
+        public void device_link_service_version_exchange(long major, long minor)
         {
-            PListRoot recv;
+            PListArray array;
             PListString msg;
 
             /* receive DLMessageVersionExchange from device */
-            recv = PropertyListService.Receive(sd);
-
-            /* get major and minor version number */
-            var array = recv.Root as PListArray;
+            array = PropertyListService.Receive(sd) as PListArray;
 
             msg = array[0] as PListString;
-            var value_major = array[1] as PListInteger;
-            var value_minor = array[2] as PListInteger;
 
-            //todo:validate major and minor
+            if (msg.Value != "DLMessageVersionExchange")
+                throw new DeviceLinkServiceException("Did not receive DLMessageVersionExchange from device!");
+
+            /* get major and minor version number */
+            if (array.Count < 3)
+                throw new DeviceLinkServiceException("DLMessageVersionExchange has unexpected format!");
+
+            var value_major = (array[1] as PListInteger).Value;
+            var value_minor = (array[2] as PListInteger).Value;
+
+            if (value_major > major)
+            {
+                throw new DeviceLinkServiceException(
+                    String.Format("Version mismatch: device=({0},{1}) > expected=({2},{3})",
+                    value_major, value_minor, major, minor));
+            }
+            else if (value_major == major && value_minor > minor)
+            {
+                throw new DeviceLinkServiceException(
+                    String.Format("WARNING: Version mismatch: device=(%lld,%lld) > expected=(%lld,%lld)",
+                    value_major, value_minor, major, minor));
+            }
 
             /* version is ok, send reply */
             var relay_array = new PListArray();
@@ -37,17 +53,69 @@ namespace BadApple
             relay_array.Add(new PListString("DLVersionsOk"));
             relay_array.Add(new PListInteger(minor));
 
-            var relay_plist = new PListRoot();
-            relay_plist.Root = relay_array;
-
-            PropertyListService.Send(sd, relay_plist);
+            PropertyListService.Send(sd, relay_array);
 
             /* receive DeviceReady message */
-            recv = PropertyListService.Receive(sd);
+            var ready_array = PropertyListService.Receive(sd) as PListArray;
 
-            msg = array[0] as PListString;
+            msg = ready_array[0] as PListString;
+            if (msg.Value != "DLMessageDeviceReady")
+                throw new DeviceLinkServiceException("Did not get DLMessageDeviceReady!");
+        }
 
-            return recv;
+        public void device_link_service_disconnect(string msg)
+        {
+            PListArray array = new PListArray();
+            array.Add(new PListString("DLMessageDisconnect"));
+
+            if (msg != null)
+                array.Add(new PListString(msg));
+            else
+                array.Add(new PListString("___EmptyParameterString___"));
+
+            PropertyListService.Send(this.sd, array);
+        }
+
+        public void device_link_service_send_ping(string msg)
+        {
+            PListArray array = new PListArray();
+            array.Add(new PListString("DLMessagePing"));
+            array.Add(new PListString(msg));
+
+            PropertyListService.Send(this.sd, array);
+        }
+
+        public void device_link_service_send_process_message(PListDict msg)
+        {
+            PListArray array = new PListArray();
+            array.Add(new PListString("DLMessageProcessMessage"));
+            array.Add(msg);
+
+            PropertyListService.Send(this.sd, array);
+        }
+
+        public IPListElement device_link_service_receive_process_message()
+        {
+            var array = PropertyListService.Receive(this.sd) as PListArray;
+
+            if (array.Count != 2)
+                throw new DeviceLinkServiceException("Malformed plist received for DLMessageProcessMessage");
+
+            var msg = array[0] as PListString;
+            if (msg.Value != "DLMessageProcessMessage")
+                throw new DeviceLinkServiceException("Did not receive DLMessageProcessMessage as expected!");
+
+            return array[1];
+        }
+
+        public void device_link_service_send(IPListElement plist)
+        {
+            PropertyListService.Send(this.sd, plist);
+        }
+
+        public IPListElement device_link_service_receive()
+        {
+            return PropertyListService.Receive(this.sd);
         }
     }
 
--- BadApple/MobileSync.cs
+++ BadApple/MobileSync.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PList;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,12 +8,124 @@ namespace BadApple
 {
     public class MobileSync
     {
-        IntPtr sd;
+        DeviceLinkService dls;
+
+        string data_class;
+        mobilesync_sync_direction_t direction;
+
         object sync = new object();
 
         public MobileSync(IntPtr sd)
         {
-            this.sd = sd;
+            this.dls = new DeviceLinkService(sd);
+            this.direction = mobilesync_sync_direction_t.MOBILESYNC_SYNC_DIR_DEVICE_TO_COMPUTER;
+            this.data_class = null;
+
+            this.dls.device_link_service_version_exchange(300, 100);
         }
+
+        public IPListElement mobilesync_receive()
+        {
+            return this.dls.device_link_service_receive();
+        }
+
+        public void mobilesync_send(IPListElement plist)
+        {
+            this.dls.device_link_service_send(plist);
+        }
+
+        public void mobilesync_start(string data_class, mobilesync_anchors_t anchors, long computer_data_class_version, out mobilesync_sync_type_t sync_type, out long device_data_class_version, out string error_description)
+        {
+            PListArray msg;
+
+            error_description = null;
+
+            msg = new PListArray();
+            msg.Add(new PListString("SDMessageSyncDataClassWithDevice"));
+            msg.Add(new PListString(data_class));
+
+            if (anchors.device_anchor != null)
+                msg.Add(new PListString(anchors.device_anchor));
+            else
+                msg.Add(new PListString("---"));
+
+            msg.Add(new PListString(anchors.computer_anchor));
+            msg.Add(new PListInteger(computer_data_class_version));
+            msg.Add(new PListString("___EmptyParameterString___"));
+
+            mobilesync_send(msg);
+
+            msg = mobilesync_receive() as PListArray;
+
+            var response_type = msg[0] as PListString;
+            if (response_type.Value == "SDMessageRefuseToSyncDataClassWithComputer")
+            {
+                var error = msg[2] as PListString;
+
+                error_description = error.Value;
+
+                throw new MobileSyncException(String.Format("Device refused sync: {0}", error.Value));
+            }
+
+            if (response_type.Value == "SDMessageCancelSession")
+            {
+                var error = msg[2] as PListString;
+
+                error_description = error.Value;
+
+                throw new MobileSyncException(String.Format("Device cancelled: {0}", error.Value));
+            }
+
+            var sync_type_node = msg[4] as PListString;
+            if (sync_type_node == null)
+                throw new MobileSyncException();
+
+            if (sync_type_node.Value == "SDSyncTypeFast")
+            {
+                sync_type = mobilesync_sync_type_t.MOBILESYNC_SYNC_TYPE_FAST;
+            }
+            else if (sync_type_node.Value == "SDSyncTypeSlow")
+            {
+                sync_type = mobilesync_sync_type_t.MOBILESYNC_SYNC_TYPE_SLOW;
+            }
+            else
+            {
+                throw new MobileSyncException();
+            }
+
+            var device_data_class_version_node = msg[5] as PListInteger;
+            device_data_class_version = device_data_class_version_node.Value;
+        }
+    }
+
+    public enum mobilesync_sync_direction_t
+    {
+        MOBILESYNC_SYNC_DIR_DEVICE_TO_COMPUTER,
+        MOBILESYNC_SYNC_DIR_COMPUTER_TO_DEVICE
+    }
+
+    public struct mobilesync_anchors_t
+    {
+        public string device_anchor;
+        public string computer_anchor;
+    }
+
+    public enum mobilesync_sync_type_t
+    {
+        MOBILESYNC_SYNC_TYPE_FAST, /**< Fast-sync requires that only the changes made since the last synchronization should be reported by the computer. */
+        MOBILESYNC_SYNC_TYPE_SLOW, /**< Slow-sync requires that all data from the computer needs to be synchronized/sent. */
+        MOBILESYNC_SYNC_TYPE_RESET /**< Reset-sync signals that the computer should send all data again. */
+    }
+
+    [Serializable]
+    public class MobileSyncException : Exception
+    {
+        public MobileSyncException() { }
+        public MobileSyncException(string message) : base(message) { }
+        public MobileSyncException(string message, Exception inner) : base(message, inner) { }
+        protected MobileSyncException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context)
+            : base(info, context) { }
     }
 }
